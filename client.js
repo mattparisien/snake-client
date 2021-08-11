82d6e99011c5872b91ea708b4243a9cd5457b7ad
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-11 19:55:21 +0000

    Created a new module for TCP connectionn, I moved the connect function from play.js to client.js & exported that function so I could easily require it from play.js.

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    port: 50541,
+    host: 'localhost'
+  });
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  conn.setEncoding('utf8'); //interpret data as txt
+
+  return conn;
+};
+
+module.exports = {
+  connect
+}
--- play.js
+++ play.js
@@ -1,19 +1,3 @@
-const net = require('net');
-
-const connect = function() {
-  console.log('Connecting...')
-  const conn = net.createConnection({
-    port: 50541,
-    host: 'localhost'
-  });
-
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-
-  conn.setEncoding('utf8'); //interpret data as txt
-
-  return conn;
-};
-
+const { connect } = require('./client');
+console.log('Connecting...')
 connect();
